f941b4cd286b745036b28d0d6b6909f812f8fb30
Lucas Rocha <rochadefreitaslucas@gmail.com>
2020-11-21 20:41:41 -0300

    ajustando calculo da media de total de homens

--- questao 40.c
+++ questao 40.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
+// questão para verificar media total de idade, quantas mulheres respondeu e a media do total de homens que responderam
 int main(){
 	int i , idade, sexo, mulher = 0, idadehomem = 0, totalidade=0;
 	float mediaidade = 0, mediaidadehomens =0;
 	int totalpessoas = 3;
 	
 	for( i = 0 ; i<totalpessoas; i++){
-		printf("Sua idade? \n");
+		printf("Sua idade? (%i -  pessoa) \n", i+1);
 		scanf("%i", &idade);
-		printf("qual o seu sexo? digite 0 para homen e 1 para mulher \n");
+		printf("qual o seu sexo? digite 0 para homen e 1 para mulher  (%i - pessoa) \n", i+1);
 		scanf("%i", &sexo);
 		
 		if(sexo){
@@ -19,7 +19,7 @@ int main(){
 		totalidade += idade;
 	}
 	mediaidade = (float) totalidade/totalpessoas;
-	mediaidadehomens = (float )(idadehomem/(totalpessoas - mulher));
+	mediaidadehomens = ((float ) idadehomem/(totalpessoas - mulher));
 	printf("total de mulheres %i \n", mulher);
 	printf("media de idade %.2f \n", mediaidade );
 	printf("media da idade dos homens: %.2f " , mediaidadehomens);
