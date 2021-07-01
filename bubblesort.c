b1bed3d40c806e1c9b1ba463a884fbb9a8a1eea1
Lucas Rocha <rochadefreitaslucas@gmail.com>
2021-06-30 23:59:38 -0300

    bubblesort com recursividade

---
+++ bubblesort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+operacaoj(int i, int j ,int *vet, int n){
+	int aux;
+	if(j < n){
+		if(vet[i] > vet[j]){
+			aux= vet[i];
+			vet[i]   = vet[j];
+			vet[j] = aux;
+		}
+		j++;
+		operacaoj(i,j, vet, n);
+	}
+} 
+
+operacaoi(int i, int *vet, int n){
+	if(i < n){
+		operacaoj(i, i+1, vet, n);
+		i++;
+		operacaoi(i, vet, n);
+	}
+}
+void bubbleSort(int *vet, int n){
+	operacaoi(0, vet, n);
+	
+}
+
+int main(){
+	int n = 8;
+    int vet[8] = {10,7,6,55,9,3,5,2};
+    bubbleSort(vet, n);
+    
+    printf ("formatado \n");
+  	 int i ; 
+  	 for(i = 0 ; i< n ; i++){
+  	 	printf("%d - ", vet[i]);
+	   }
+   	return 0;
+}
